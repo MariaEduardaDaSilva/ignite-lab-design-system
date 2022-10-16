36448d46d0bca0c69b13947595002aff19aa97d0
MariaEduardaDaSilva <mariaeduda941@gmail.com>
2022-10-16 18:07:55 -0300

    ci: Main problem fixed

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,12 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
-  viteFinal: (config, { configType}) => {
-    if (configType === 'PRODUCTION') {
-      config.bse = '/ignite-lab-design-system/'
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType == 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system'
     }
 
-    return config
+    return config;
   }
 }
